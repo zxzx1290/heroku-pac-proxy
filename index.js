d53ad4d34463b7276b0d7127c19f128927aede26
zxzx1290 <indexzexx@gmail.com>
2017-10-26 20:42:08 +0800

    add openstreetmap opt

--- index.js
+++ index.js
@@ -10,24 +10,34 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/', function(req, res) {
-  request({
-      method: 'GET',
-      url: 'http://pac.uku.im/regex',
-  },function(error, ret, body){
-      res.send(body);
-  });
+    request({
+        method: 'GET',
+        url: 'http://pac.uku.im/regex',
+    }, function(error, ret, body) {
+        res.send(body);
+    });
 });
 
 app.get('/openstreetmap', function(req, res) {
-  request({
-      method: 'GET',
-      url: 'http://polygons.openstreetmap.fr/get_geojson.py?id='+req.query.id+"&params=0",
-  },function(error, ret, body){
-      res.send(body);
-  });
+    if(isNaN(req.query.id) ){
+        res.end("error");
+        return;
+    }
+    request({
+        method: 'GET',
+        url: 'http://polygons.openstreetmap.fr/get_geojson.py?id=' + req.query.id + "&params=0",
+    }, function(error, ret, body) {
+        res.send(body);
+    });
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
